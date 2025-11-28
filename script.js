bd4cec885bd71753b726e44f390524e91a4df912
chanchal887 <thakurchanchal0726@gmail.com>
2025-11-28 13:14:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let p = document.querySelector("p")
+let input = document.querySelector("input");
+let btn = document.querySelector("button");
+let box = document.querySelector(".box")
+const currntdt = new Date;
+
+
+btn.addEventListener("click",(e)=>{
+  let year = currntdt.getFullYear();
+  let month = currntdt.getMonth();
+  let date = currntdt.getDate()
+
+  let val =(date,month,year)
+   let inp = parseInt(input.value)-val
+   console.log(-inp);
+   p.innerHTML=`your age is ${-inp}`;
+
+   
+   if(input.value==""){
+    alert("please first enter the value")
+    p.innerHTML=""
+   }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function createlm(){
+  let total=  document.createElement("p");
+    total.innerHTML=`your age is`
+    box.append(total)
+}
